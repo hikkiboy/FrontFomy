a773212d3af1f6d0a53c32d2a09c073399772f44
hikkiboy <gustavoht2014@gmail.com>
2023-10-10 21:50:12 -0300

    n mudei mt coisa

--- Fomy/app/screens/home/index.js
+++ Fomy/app/screens/home/index.js
@@ -1,4 +1,4 @@
-import {View, Text, Button, FlatList} from 'react-native'
+import {View, Text, Button, FlatList, TouchableOpacity} from 'react-native'
 import { app_auth, app_DB } from '../../../firebaseConfig'
 import { doc , collection, query, where, onSnapshot, Firestore, documentId} from 'firebase/firestore'
 import { useEffect, useState} from 'react'
@@ -8,66 +8,52 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Home = ({navigation}) => {
 
+ 
+
     const [Receitas, setReceitas] = useState([]);
 
-    useEffect(()=>{
+    
 
-        
-    
-        const receitaRef = collection(app_DB, 'Usuarios')
-    
-        const q = query(
-            receitaRef,
-            where(documentId(), '==', app_auth.currentUser.uid)
-        )
-    
-        
-    
-        const subscriver = onSnapshot(q, {
-            next : (snapshot) => {
-                const receitas = []
-                
-                snapshot.docs.forEach(doc =>{   
-                    receitas.push({
-                        key : doc.id,
-                        ...doc.data(),
-                       
-                    })
+useEffect(()=>{
+    const receitaRef = collection(app_DB, 'Trilhas')
+
+    const subscriver = onSnapshot(receitaRef, {
+        next : (snapshot) => {
+            const receitas = []
+            snapshot.docs.forEach(doc =>{
+                console.log(doc.data())
+                receitas.push({
+                    key : doc.id,
+                    ...doc.data()
                 })
-                setReceitas(receitas)
-                console.log(receitas)
-                console.log(receitaRef)
+            })
+            setReceitas(receitas)
+            console.log(receitas)
+            console.log(Receitas)
 
-    
-            }
-        })
-    
-        return() => subscriver()
-    
-    },[])
-    
+        }
+    })
 
-    return (
-        
-        <View>
+    return() => subscriver()
 
-<FlatList
+},[])
+
+
+return(
+  <FlatList
   data={Receitas}
   renderItem={({item}) => (
     <SafeAreaView>
-
-        <Text>Nome: {item.Nome}</Text>
-    </SafeAreaView>
+        <TouchableOpacity onPress={() => navigation.navigate(item.NomeTrilha)}>
+        <Text>Trilha: {item.NomeTrilha}</Text>
+        </TouchableOpacity>
+        </SafeAreaView>
   )}
-  />        
-            <SafeAreaView>
-            <Button onPress={() =>{app_auth.signOut()}} title = "Sair"/>
-            <Button onPress={() => navigation.navigate('Fetch')} title = "Trilhas"/>
-            </SafeAreaView>
-            
+  />
+)
 
-        </View>
-    )
 }
+
+    
 
 export default Home
---
+++ Fomy/app/screens/profile/index.js
@@ -0,0 +1,73 @@
+import {View, Text, Button, FlatList, TouchableOpacity} from 'react-native'
+import { app_auth, app_DB } from '../../../firebaseConfig'
+import { doc , collection, query, where, onSnapshot, Firestore, documentId} from 'firebase/firestore'
+import { useEffect, useState} from 'react'
+import auth from '@react-native-firebase/auth'
+
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Profile = ({navigation}) => {
+
+    const [Receitas, setReceitas] = useState([]);
+
+    useEffect(()=>{
+
+        
+    
+        const receitaRef = collection(app_DB, 'Usuarios')
+    
+        const q = query(
+            receitaRef,
+            where(documentId(), '==', app_auth.currentUser.uid)
+        )
+    
+        
+    
+        const subscriver = onSnapshot(q, {
+            next : (snapshot) => {
+                const receitas = []
+                
+                snapshot.docs.forEach(doc =>{   
+                    receitas.push({
+                        key : doc.id,
+                        ...doc.data(),
+                       
+                    })
+                })
+                setReceitas(receitas)
+                console.log(receitas)
+                console.log(receitaRef)
+
+    
+            }
+        })
+    
+        return() => subscriver()
+    
+    },[])
+    
+
+    return (
+        
+        <View>
+
+<FlatList
+  data={Receitas}
+  renderItem={({item}) => (
+    <SafeAreaView>
+
+        <Text>Nome: {item.Nome}</Text>
+    </SafeAreaView>
+  )}
+  />        
+            <SafeAreaView>
+            <Button onPress={() =>{app_auth.signOut()}} title = "Sair"/>
+            {/*<Button onPress={() => navigation.navigate('Fetch')} title = "Trilhas"/>*/}
+            </SafeAreaView>
+            
+
+        </View>
+    )
+}
+
+export default Profile
