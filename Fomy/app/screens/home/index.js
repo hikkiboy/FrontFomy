bb212509021d5a08ce193a1f1dc7d6bfabda64e1
hikkiboy <gustavoht2014@gmail.com>
2023-10-16 21:51:42 -0300

    Home is kinda things

--- Fomy/app/screens/home/index.js
+++ Fomy/app/screens/home/index.js
@@ -1,9 +1,9 @@
-import {View, Text, Button, FlatList, TouchableOpacity} from 'react-native'
+import {View, Button, FlatList, TouchableOpacity,StyleSheet, Dimensions} from 'react-native'
 import { app_auth, app_DB } from '../../../firebaseConfig'
 import { doc , collection, query, where, onSnapshot, Firestore, documentId} from 'firebase/firestore'
 import { useEffect, useState} from 'react'
 import auth from '@react-native-firebase/auth'
-
+import {Canvas, Circle, Group, LinearGradient, vec, RoundedRect, useImage, Image,Shadow, Text,useFont} from "@shopify/react-native-skia";
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Home = ({navigation}) => {
@@ -39,21 +39,51 @@ useEffect(()=>{
 },[])
 
 
-return(
-  <FlatList
-  data={Receitas}
-  renderItem={({item}) => (
-    <SafeAreaView>
-        <TouchableOpacity onPress={() => navigation.navigate(item.NomeTrilha)}>
-        <Text>Trilha: {item.NomeTrilha}</Text>
-        </TouchableOpacity>
-        </SafeAreaView>
-  )}
-  />
-)
+const Alberto = useImage(require('../../assets/alberto.png'));
+const fontSize = 32;
+  const font = useFont(require('../../assets/Roboto-Bold.ttf'), fontSize);
 
+return (
+    <>
+       <Canvas style={{ flex: 1 }}>
+    <RoundedRect   
+    x={50}
+      y={200}
+      width={256}
+      height={256}
+      r={25}
+      color="lightblue">
+    <Shadow dx={12} dy={12} blur={0} color="#93b8c4" />
+      </RoundedRect>
+    <Text
+    x={100}
+    y={350}
+    text="Os Basicos"
+    font = {font}
+    
+    />
+   
+
+    {Alberto && (
+        <Image
+        image={Alberto}
+        fit="contain"
+        x={150}
+        y={250}
+        width={64}
+        height={64}
+        />
+    )}
+  </Canvas>
+      <Button title="teste" onPress={() => navigation.navigate(Receitas[0].NomeTrilha)} />
+    </>
+  );
 }
 
-    
+const styles = StyleSheet.create({
+    container: {
+        flex : 1
+    }
+})
 
 export default Home
