3c07a4f61a85a9e5aebaa1583562ca2aff33100e
hikkiboy <gustavoht2014@gmail.com>
2023-10-18 19:29:01 -0300

    p

--- Fomy/app/components/paginator/index.js
+++ Fomy/app/components/paginator/index.js
@@ -5,6 +5,9 @@ import {View, StyleSheet, Text} from 'react-native'
 export default paginator = ({data}) =>{
     return (
         <View style={{flexDirection: 'row', height: 64}}>
+         {data.map((_,i) => {
+            return <View style={[styles.dot, {width: 10}]} key={i.toString()}/>
+         })}
            
         </View>
     )
