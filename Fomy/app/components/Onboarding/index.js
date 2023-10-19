2e6f3138db5b12908059cb6875814688b5e0d3a0
hikkiboy <gustavoht2014@gmail.com>
2023-10-18 21:53:04 -0300

    tela home ta bugada ainda vou ficar louco

--- Fomy/app/components/Onboarding/index.js
+++ Fomy/app/components/Onboarding/index.js
@@ -1,12 +1,54 @@
 import { View, Text, Image, useWindowDimensions, StyleSheet, Button} from 'react-native'
-import React from 'react'
-import { app_auth, app_DB } from '../../../firebaseConfig'
+import * as Progress from 'react-native-progress' 
+import {useEffect, useState} from 'react'
 import { doc , collection, query, where, onSnapshot, Firestore, documentId} from 'firebase/firestore'
-import { useState, useEffect } from 'react';
-import { BlurMask, center } from '@shopify/react-native-skia';
+import { app_auth, app_DB } from '../../../firebaseConfig'
+
+
 
 
 const OnboardingItem = ({item, navigation}) => {
+
+    
+    const [Usuarios, setUsuarios] = useState([]);
+
+    useEffect(()=>{
+
+        
+    
+        const receitaRef = collection(app_DB, 'Usuarios')
+    
+        const q = query(
+            receitaRef,
+            where(documentId(), '==', app_auth.currentUser.uid)
+        )
+    
+        
+    
+        const subscriver = onSnapshot(q, {
+            next : (snapshot) => {
+                const receitas = []
+                
+                snapshot.docs.forEach(doc =>{   
+                    receitas.push({
+                        key : doc.id,
+                        ...doc.data(),
+                       
+                    })
+                })
+                setUsuarios(receitas)
+                //console.log(receitas)
+                //console.log(receitaRef)
+
+    
+            }
+        })
+    
+        return() => subscriver()
+    
+    },[])
+    
+    //console.log(Usuarios[0].ProgressoTrilhas);
 
     const {width} = useWindowDimensions()
 
@@ -17,9 +59,14 @@ const OnboardingItem = ({item, navigation}) => {
       <Image source={require('../../assets/alberto.png')} style={styles.mascote}/>
       
         <Text style={styles.title}>{item.NomeTrilha}</Text>
-        <Text>{item.Descricao}</Text>
-       
-        <Button title='Entrar' onPress={navigation.navigate}/>
+        <Text style={styles.description}>{item.Descricao}</Text>
+        
+        <View style={{paddingTop: 10, paddingBottom:20}}>
+        {/*<Progress.Bar style={{alignSelf:'center'}} progress={Usuarios[0].ProgressoTrilhas[item.indexTrilha]} width={250} height={20} color='#32a852'><Text style={{position:'absolute', flex:0, alignSelf: 'center'}}>{Usuarios[0].ProgressoTrilhas[item.indexTrilha] * 10}/10</Text></Progress.Bar>*/}
+        </View>
+        <View>
+        {/*<Button title='Entrar' onPress={navigation.navigate(item.NomeTrilha)}/>*/}
+        </View>
    
     </View>
 
@@ -51,8 +98,7 @@ const styles = StyleSheet.create({
     },
     description:{
         fontWeight: '300',
-        fontSize: 28,
-        paddingHorizontal:64,
+        fontSize: 20,
         textAlign:'center'
     },
     mascote:{
@@ -67,7 +113,8 @@ const styles = StyleSheet.create({
         width: 200,
         backgroundColor:'black',
         marginTop: 160
-    }
+    },
+
 })
 
 
