6c7b24405179765dcaa731038855b42af29c70c6
Furover <3264.343@educacao.org.br>
2023-11-14 21:52:55 -0300

    steps screen lokin finnee

--- Fomy/app/components/passos/index.js
+++ Fomy/app/components/passos/index.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image,FlatList,TouchableWithoutFeedback, TouchableOpacity, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image,FlatList,TouchableWithoutFeedback, TouchableOpacity, ImageBackground, ScrollView } from 'react-native';
 import Modal from "react-native-modal";
 import { app, app_DB } from '../../../firebaseConfig'
 import { collection, onSnapshot, query, where, orderBy,documentId, collectionGroup } from '@firebase/firestore'
@@ -7,6 +7,7 @@ import React, { useEffect, useState } from 'react'
 import { Button, ListItem } from 'react-native-elements';
 import { center } from '@shopify/react-native-skia';
 import { Feather } from 'react-native-vector-icons'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 export default function Passos({route, props, navigation}) {
@@ -65,7 +66,7 @@ function pa(i, fwd){
     setPasso(Receitas[(i - 1)]);
     console.log("Valor: "+ i + "Valor2: "+calcula);
 
-  } else if(i < Receitas.length && fwd == false){
+  } else if(fwd == false && i - 1 != 0){
     i--;
     setCalcula(i);
     setPasso(Receitas[(i - 1)]);
@@ -80,57 +81,126 @@ function pa(i, fwd){
 console.log("Passo: ",Passo)
 
   return (
-       <SafeAreaView style={{marginTop: 30}}>
-        <TouchableOpacity style={{ position: 'absolute', zIndex: 99 }} ><Feather name="chevron-left" color={"black"} size={40} /></TouchableOpacity>
-        <ImageBackground style={{height: 250, zIndex: 0}} source={require('../../assets/Group171.png')}>
-        <View style={styles.botaoPassos}>
-          <Text style={styles.TituloPasso}> Passo {Passo.Sequencia}:  {Passo.Titulo}</Text>
-          <Button onPress={ () => pa(calcula, true)}></Button>
-          
+       <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <ImageBackground style={styles.imagebak} source={require('../../assets/Group171.png')}>
+            <TouchableOpacity onPress={ () => navigation.goBack() } style={styles.goback} ><Feather name="chevron-left" color={"black"} size={40} /></TouchableOpacity>
+            <View style={styles.areatitulo}>
+              <Text style={styles.titulopasso}> Passo {Passo.Sequencia}:  {Passo.Titulo}</Text>
+              
+            </View>
+            <View style={styles.video}>
+            
+            </View>
+          </ImageBackground>
+          <View style={styles.belowimage} >
+            <View style={styles.teacharea} >
+              <Text style={styles.descpasso} >{Passo.Passo}</Text>
+              <Image style={styles.bubbleimage} source={require("../../assets/bubbleTriangle.png")} />
+              <Image style={styles.charimage} source={require("../../assets/betterAlberto.png")} />
+            </View>
+            <View style={styles.buttons} >
+              <TouchableOpacity style={styles.stepbak} onPress={() => pa(calcula, false) } ><Feather name={"arrow-left"} size={40} /></TouchableOpacity>
+              <TouchableOpacity style={styles.stepfwd} onPress={() => pa(calcula, true) } ><Feather name={"arrow-right"} size={40} /></TouchableOpacity>
+            </View>
           </View>
-          <View style={styles.Video}>
-          
-          </View>
-        </ImageBackground>
-        <View>
-        
-        </View>
+        </ScrollView>
        </SafeAreaView>  
       )} 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, 
     display: 'flex',
+    backgroundColor: "#FFF"
     
   },
-  botaoPassos:{
+  goback:{
+    position: 'absolute', 
+    zIndex: 99, 
+    marginTop: 10, 
+    marginLeft: 3
+  },
+  imagebak:{
+    height: 210, 
+    zIndex: 0,
+    backgroundColor: 'white'
+  },
+  areatitulo:{
     width: '90%',
     height:30,
-    borderWidth: 1, 
     borderRadius: 20,
     backgroundColor: '#5D875D',
     alignSelf: 'center',
-    marginTop: '30%'
+    marginTop: 65,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  TituloPasso:{
+  titulopasso:{
     fontSize: 20,
     textAlign: 'center',
     fontWeight: 'bold',
-    color: '#FFFFFF'
+    color: '#FFFFFF',
+
   },
-  Video:{
+  video:{
     backgroundColor: 'black',
     height: 200,
-    width: 300,
+    width: '90%',
     alignSelf: 'center',
     borderRadius: 20,
     marginTop: 20
   },
-  DescPasso:{
-    fontSize: 20,
+  belowimage:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    marginTop: 150,
+    backgroundColor: "#FFF"
+  },
+  teacharea:{
+    width: '90%',
+    alignItems: 'center'
+    
+  },
+  descpasso:{
+    fontSize: 18,
     textAlign: 'center',
-    fontWeight: 'bold',
-    color: '#000000'
+    fontWeight: '600',
+    color: '#000000',
+    width: '100%',
+    borderRadius: 15,
+    borderWidth: 2,
+    padding: 5
+  },
+  charimage:{
+    height: 144,
+    width: 119,
+    marginTop: 15
+  },
+  bubbleimage:{
+    height: 75,
+    width: 59,
+    marginTop: -30
+  },
+  buttons:{
+    justifyContent: 'center', 
+    flexDirection: 'row',
+    marginTop: 35
+
+  },
+  stepbak:{
+    marginBottom: 35,
+    marginHorizontal: 35,
+    padding: 20,
+    backgroundColor: "#F68F92",
+    borderRadius: 15
+  },
+  stepfwd:{
+    marginBottom: 35,
+    marginHorizontal: 35,
+    padding: 20,
+    backgroundColor: "#7EB77F",
+    borderRadius: 15
   }
  
 
