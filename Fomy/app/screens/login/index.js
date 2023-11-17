d9a5d546630905929694272e51726f355b38a2c3
Furover <133924982+Furover@users.noreply.github.com>
2023-11-17 16:19:28 +0000

    Removed default login AGAIN

--- Fomy/app/screens/login/index.js
+++ Fomy/app/screens/login/index.js
@@ -24,8 +24,8 @@ import { useNavigation } from "@react-navigation/native";
 
 const LoginPage = () => {
   const navigation = useNavigation();
-  const [email, setEmail] = useState("testando@essabudega.com");
-  const [senha, setSenha] = useState("thissucks");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
   
   const [loading, setLoading] = useState(false);
   const auth = app_auth;
