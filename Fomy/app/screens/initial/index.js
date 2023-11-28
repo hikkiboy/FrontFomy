c2d06c81479d781aabb6390cd1a5d7787905e145
Furover <fur.wiggywag@protonmail.com>
2023-11-28 14:13:29 -0300

    Initial is now a bit responsive

--- Fomy/app/screens/initial/index.js
+++ Fomy/app/screens/initial/index.js
@@ -1,34 +1,51 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView, Alert } from 'react-native';
-import React, {useState} from 'react'
+import { StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView, Alert, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react'
 import { app_auth } from '../../../firebaseConfig'
 import { Logo } from '../../components/logo';
-import {AntDesign} from '@expo/vector-icons'; 
+import { AntDesign } from '@expo/vector-icons'; 
 
 
 
 
 const Login = ({navigation}) => {
+  const height = Dimensions.get("window").height
+  const [stuffHeight, setStuffHeight] = useState(90)
+  const [imageHeight, setImageHeight] = useState(225.05)
+  const [imageWidth, setImageWidth] = useState(207.2)
+  const [fontSize, setFontSize] = useState(22)
+  const [googleHeight, setGoogleHeight] = useState(32.64)
+  const [googleWidth, setGoogleWidth] = useState(99.14)
+  
+  useEffect(() => {
+    if(height <= 680){
+      console.log("tela pequena")
+      setStuffHeight(75)
+      setImageHeight(180.04)
+      setImageWidth(165.76)
+      setFontSize(18)
+      setGoogleHeight(26.112)
+      setGoogleWidth(79.312)
+    }
+  })
 
     return (
         <SafeAreaView style={styles.container} >
 
-          <Image style={styles.logo} source={require("../../assets/logo-full.png")} />
+          <Image style={[styles.logo, { width: imageWidth, height: imageHeight }]} source={require("../../assets/logo-full.png")} />
     
-          <TouchableOpacity style={styles.buttonCadastro} onPress={() => navigation.navigate('Cadastro')}>
-            <Text style={styles.loginCadastro}>Cadastro</Text>
+          <TouchableOpacity style={[styles.buttonCadastro, { height: stuffHeight }]} onPress={() => navigation.navigate('Cadastro')}>
+            <Text style={[styles.login, {fontSize: fontSize}]}>Cadastro</Text>
           </TouchableOpacity>
     
-          <TouchableOpacity style={styles.buttonLogin} onPress={ () => navigation.navigate('Loginpage')}>
-            <Text style={styles.login}>Login</Text>
+          <TouchableOpacity style={[styles.buttonLogin, { height: stuffHeight }]} onPress={ () => navigation.navigate('Loginpage')}>
+            <Text style={[styles.login, {fontSize: fontSize}]}>Login</Text>
           </TouchableOpacity>
     
-          <Text style={styles.otherOptions}>-- ou entre com --</Text>
+          <Text style={[styles.otherOptions, { fontSize: (fontSize - 1) }]}>-- ou entre com --</Text>
 
-            <TouchableOpacity style={styles.buttonCadastroGoogle} onPress={ () => navigation.navigate('Loginpage')}>
-              <AntDesign style={styles.iconeGoogle} name="google" size={28} />
-              <Text style={[styles.loginGoogle]}>oogle</Text>
-            
-            </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonCadastroGoogle} onPress={ () => navigation.navigate('Loginpage')}>
+            <Image tintColor={"#FFF"} style={[{ width: googleWidth, height: googleHeight }]} source={require("../../assets/fullLogoGoogle.png")} />
+          </TouchableOpacity>
   
         
       </SafeAreaView>
@@ -50,57 +67,42 @@ const styles = StyleSheet.create({
 
   },
   logo:{
-    width: 207.2,
-    height: 225.05,
-    marginBottom: 45,
+    marginBottom: "10%",
     resizeMode: 'stretch'
   },
   buttonCadastro: {
     backgroundColor: '#A4CCA4',
-    padding: 12,
     width: "85%",
+    alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 15,
     borderColor: '#8eb28e',
     borderBottomWidth: 8,
     borderWidth: 5
   },
-  loginCadastro:{
-    alignSelf: 'center',
-    fontSize: 22,
-    fontWeight: 'bold',
-    padding: 10,
-    paddingLeft: 40,
-    paddingRight: 40,
-    opacity: 0.5,
-  },
   buttonLogin: {
     backgroundColor: '#FFF',
-    padding: 12,
     width: "85%",
+    alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 15,
     borderColor: '#dbdbdb',
     borderBottomWidth: 8,
     borderWidth: 5,
-    marginTop: 20
+    marginTop: "5%"
     
   },
   login:{
-    alignSelf: 'center',
-    fontSize: 22,
     fontWeight: 'bold',
-    padding: 12,
-    paddingLeft: 57,
-    paddingRight: 57,
     opacity: 0.5,
   },
   loginGoogle:{
     fontSize: 28,
-    fontWeight: 'bold',
     color: "#FFF"
   },
   buttonCadastroGoogle: {
     backgroundColor: '#F68F92',
-    padding: 10,
+    padding: 11,
     width: "85%",
     borderRadius: 15,
     borderColor: '#e88689',
@@ -117,16 +119,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   otherOptions:{
-    top: 30,
     textAlign: 'center',
-    fontSize: 20,
     opacity: 0.3,
     width:'100%',
-    marginBottom: 50
+    marginVertical: "7%"
     },
-    iconeGoogle:{
-      color: "#FFF"
-    }
   
 });
 
