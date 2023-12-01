97d2c19691819f083a1ec8b6736f1151003b8f23
PAULO SILAS DA SILVA SANTOS <3276.343@edu.fb.org.br>
2023-11-30 21:48:53 -0300

    Preparo

--- Fomy/app/components/preparo/index.js
+++ Fomy/app/components/preparo/index.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image,FlatList,TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image,FlatList,TouchableWithoutFeedback, TouchableOpacity, ScrollView } from 'react-native';
 import Modal from "react-native-modal";
 import { app, app_DB } from '../../../firebaseConfig'
 import { collection, onSnapshot, query, where, orderBy,documentId } from '@firebase/firestore'
@@ -59,9 +59,10 @@ let arrayUtil = []
 let arrayprep = []
 let arrayporc = []
 
+
   return (
     <SafeAreaView style={styles.container} >
-      
+      <ScrollView>
     {/* //   <View style={{backgroundColor: route.params.paramKey[2],marginTop: 14, width: 378, height: 219, borderRadius:15 }}>
     //   <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
     //     <Image style={{width:109, height:109, marginTop: 18}} source={require('../../assets/fogao.png')}/>
@@ -76,17 +77,37 @@ let arrayporc = []
    
       {/* <View style={styles.linha}></View> */}
 {/* fazer um flat list pra gerar as fases  */}
-<View style={styles.bgpfp} >
+<View style={[styles.bgpfp, {backgroundColor: route.params.paramKey[1],}]} >
+    
+      <Image 
+      tintColor={"#FFF"}
+      source={{ uri: route.params.paramKey[2] }} 
+      style={{height: 100, width: 100, marginTop: 50}}
+      />
     </View>
-      <FlatList
+      
+      <FlatList nestedScrollEnabled
+
+
       data={Receitas}
       scrollEnabled = {true}
       showsVerticalScrollIndicator ={false}
       
+      
       renderItem={({item}) => (
     <SafeAreaView style={styles.container}>
+
+
+    
       {/* <Button onPress={() => console.log(item)}></Button> */}
-        <View style={styles.nomebg}>
+        <View style={[{
+              backgroundColor: route.params.paramKey[1],
+              width: '70%',
+              borderRadius: 100,
+              marginTop: '3%',
+              marginBottom: '2%',
+              alignSelf: 'center'
+        }]}>
         <Text style={styles.nomeTexto}>{item.Nome}</Text>
         </View>
         <View style={styles.ContainTextAndIcon}>
@@ -102,7 +123,7 @@ let arrayporc = []
             <Text style={{opacity: 0, position: 'absolute'}}>{arrayporc.push(item.Porcoes)}</Text>
         <View style={styles.IngredientesContain}>
             
-            <FlatList
+            <FlatList nestedScrollEnabled
             data={arrayIng[0]}
             scrollEnabled = {true}
             showsVerticalScrollIndicator = {false}
@@ -121,7 +142,7 @@ let arrayporc = []
              {arrayBon.includes(undefined) != true && (
               <>
               <View>
-                <Button title= 'debug' onPress={() => console.log(arrayBon)}></Button>
+              
               <View style={{display: 'flex', flexDirection: 'row', marginTop: 30}}>
               <Image style={styles.Ovo} source={require('../../assets/Ovo_leite.png')}/>
               <View style={styles.BonusBg}>
@@ -133,7 +154,7 @@ let arrayporc = []
               </View>
             </View>
 
-             <FlatList
+             <FlatList nestedScrollEnabled
             data={arrayBon[0]}
             scrollEnabled = {true}
             showsVerticalScrollIndicator = {false}
@@ -153,7 +174,7 @@ let arrayporc = []
               <Text style={styles.UtilTexto}>Utensílios</Text>
               </View>
             </View>
-             <FlatList
+             <FlatList nestedScrollEnabled
             data={arrayUtil[0]}
             scrollEnabled = {true}
             showsVerticalScrollIndicator = {false}
@@ -178,7 +199,7 @@ let arrayporc = []
           </TouchableOpacity>
         </View>
         </View>
-          <FlatList
+          <FlatList nestedScrollEnabled
             data={arrayprep[0]}
             scrollEnabled = {true}
             showsVerticalScrollIndicator = {false}
@@ -192,8 +213,9 @@ let arrayporc = []
         </View>
         
      </SafeAreaView>
+     
       )}
-      />
+      /></ScrollView>
        </SafeAreaView>
   );
 }
@@ -220,8 +242,8 @@ const styles = StyleSheet.create({
     alignSelf: 'center'
   },
   bgpfp:{
-    height: '30%',
-    backgroundColor: "#7EB77F",
+    height: 170,
+    width: 393,
     alignItems: 'center',
     paddingBottom: 40,
     borderBottomStartRadius: 20,
