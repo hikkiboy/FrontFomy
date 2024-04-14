b3b84d5379450aa39a3f1a5b9a2ddf3e1eff2564
hikki <98045812+hikkiboy@users.noreply.github.com>
2024-04-14 17:23:52 -0300

    Update firebaseConfig.js
    something somethign

--- Fomy/firebaseConfig.js
+++ Fomy/firebaseConfig.js
@@ -9,14 +9,7 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import 'firebase/firestore'
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBUMWIctMwmBBp_Hl2MzXK2wpJckCkZeT8",
-  authDomain: "fomy-5ea9c.firebaseapp.com",
-  projectId: "fomy-5ea9c",
-  storageBucket: "fomy-5ea9c.appspot.com",
-  messagingSenderId: "27576730639",
-  appId: "1:27576730639:web:d8e04715ded327539c0f7a"
-};
+//api key goes here
 
 // Initialize Firebase
 export const app = firebase.initializeApp(firebaseConfig);
