5c31276357bdc97547e132af0d7d79d2725dd193
Furover <suspicious.porcupine379@passinbox.com>
2024-06-22 19:27:25 -0300

    We have fonts boys!!!! WE HAVE CUSTOM FONTSSSS

--- Fomy/App.js
+++ Fomy/App.js
@@ -1,15 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import { User, onAuthStateChanged } from "firebase/auth";
+import { useFonts, Fredoka_300Light, Fredoka_400Regular, Fredoka_500Medium, Fredoka_600SemiBold, Fredoka_700Bold } from '@expo-google-fonts/fredoka';
 
 import { useEffect, useState } from "react";
 import { app_auth } from "./firebaseConfig";
 
 import Routes, { TabNavigatior } from "./app/routes/stackRouter";
-import LoginPage from "./app/screens/login";
-import Login from "./app/screens/initial";
 
 const Stack = createNativeStackNavigator();
 
@@ -19,13 +16,22 @@ export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [loading, setLoading] = useState(true);
 
+  let [fontsLoaded] = useFonts({
+    "FredokaLight": Fredoka_300Light,
+    "FredokaRegular": Fredoka_400Regular,
+    "FredokaMedium": Fredoka_500Medium,
+    "FredokaSemibold": Fredoka_600SemiBold,
+    "FredokaBold": Fredoka_700Bold
+  });
+
+
   useEffect(() => {
     const login = onAuthStateChanged(app_auth, (user) => {
-      if(user) {
+      if(user && fontsLoaded) {
         setLoggedIn(true);
         //console.log("logado");
         setLoading(false);
-      } else {
+      } else if (fontsLoaded) {
         setLoggedIn(false);
         //console.log("não tá logado");
         setLoading(false);
@@ -34,7 +40,7 @@ export default function App() {
     });
 
     return () => login();
-  }, []);
+  }, [fontsLoaded]);
 
   return (
     <NavigationContainer>
